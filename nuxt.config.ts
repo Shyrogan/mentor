67a1b83e94fe4b931f930df595376b3399bb45cc
Sébastien VIAL <sebastien@shyrogan.fr>
2024-06-03 11:10:07 +0200

    chore: add `eslint` and `prettier` alongside plugins

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"]
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', 'nuxt-icon'],
+  app: {
+    head: {
+      title: 'Mentor',
+    },
+  },
+  shadcn: {
+    prefix: '',
+    componentDir: './components/ui',
+  },
 })
